f6d256109290994cb71ed115aed0e40e36dc89c2
Jeet Mondal <jm382118@gmail.com>
2026-03-19 00:00:24 +0530

    Add Theme debug

--- seedAdmin.js
+++ seedAdmin.js
@@ -1,40 +1,74 @@
 
+//   const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// const User = require("./models/User");
+
+// dotenv.config();
+
+// const ADMIN_EMAIL = "jm382118@gmail.com";
+// const ADMIN_PASSWORD = "JeeT19JMMJ46296dBusER";
+
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(async () => {
+//     console.log("MongoDB connected...");
+
+//     const existingAdmin = await User.findOne({ email: ADMIN_EMAIL });
+
+//     if (existingAdmin) {
+//       console.log("Admin already exists with this email");
+//       process.exit();
+//     }
+
+//     await User.create({
+//       name: "System Admin",
+//       email: ADMIN_EMAIL,
+//       password: ADMIN_PASSWORD,
+//       role: "admin",
+//       isApproved: true,
+//       isActive: true,
+//     });
+
+//     console.log("Admin created successfully");
+//     console.log("Email:", ADMIN_EMAIL);
+//     console.log("Password:", ADMIN_PASSWORD);
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     process.exit(1);
+//   });
 
 
 
 
-  const mongoose = require("mongoose");
+
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const User = require("./models/User");
 
 dotenv.config();
 
-const ADMIN_EMAIL = "jm382118@gmail.com";
-const ADMIN_PASSWORD = "JeeT19JMMJ46296dBusER";
-
 mongoose.connect(process.env.MONGO_URI)
   .then(async () => {
     console.log("MongoDB connected...");
 
-    const existingAdmin = await User.findOne({ email: ADMIN_EMAIL });
+    const existingAdmin = await User.findOne({ email: "jm382118@gmail.com" });
 
     if (existingAdmin) {
-      console.log("Admin already exists with this email");
+      console.log("Admin already exists");
       process.exit();
     }
 
     await User.create({
       name: "System Admin",
-      email: ADMIN_EMAIL,
-      password: ADMIN_PASSWORD,
+      email: "jm382118@gmail.com",
+      password: "Admin@12345",
       role: "admin",
       isApproved: true,
       isActive: true,
     });
 
     console.log("Admin created successfully");
-    console.log("Email:", ADMIN_EMAIL);
-    console.log("Password:", ADMIN_PASSWORD);
     process.exit();
   })
   .catch((err) => {
